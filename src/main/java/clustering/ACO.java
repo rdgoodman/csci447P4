c3733a59aea424db9905e2f6565a07cd5321e0fe
Janette Rounds <janette.rounds@msu.montana.edu>
2015-12-03 16:18:32 -0700

    Fixed ACO!!

--- src/main/java/clustering/ACO.java
+++ src/main/java/clustering/ACO.java
@@ -18,6 +18,21 @@ public class ACO {
 	double alpha;
 	int stepsize;
 
+	
+	/**
+	 * Creates new ACO instance 
+	 * 
+	 * @param k
+	 * @param gens
+	 * @param agents
+	 * @param gridColSize
+	 * @param gridRowSize
+	 * @param neighborhoodSize
+	 * @param kPlus
+	 * @param kMinus
+	 * @param alpha
+	 * @param stepsize
+	 */
 	public ACO(int k, int gens, int agents, int gridColSize, int gridRowSize, int neighborhoodSize, double kPlus,
 			double kMinus, double alpha, int stepsize) {
 		this.k = k;
@@ -33,6 +48,7 @@ public class ACO {
 	}
 
 	public ArrayList<Cluster> run(ArrayList<Datum> data) {
+		System.out.println("initializing grid");
 		grid = new int[gridRowSize][gridColSize][2];
 		for (int i = 0; i < gridRowSize; i++) {
 			for (int j = 0; j < gridColSize; j++) {
@@ -44,7 +60,9 @@ public class ACO {
 		ArrayList<Cluster> clusters = initializeClusters(data, grid);
 
 		for (int i = 0; i < gens; i++) {// number of generations
+			System.out.println("Beginning generation " + i);
 			for (int j = 0; j < numAgents; j++) { // number of agents
+				//System.out.println("Agent " + j);
 				// move agent randomly
 				ACOAgent cur = agents.get(j);
 				int pr = cur.getRow();
@@ -58,6 +76,7 @@ public class ACO {
 				//while (move) {
 				if (dir == 0) {
 					if (grid[pr][pc1][0] == -1) {
+						//System.out.println("Moving " + j);
 						agents.get(j).setColumn(pc1);
 						grid[pr][pc1][0] = j;
 						grid[pr][pc][0] = -1;
@@ -67,6 +86,7 @@ public class ACO {
 				}
 				if(dir == 1){
 					if(grid[pr][pc2][0] == -1){
+						//System.out.println("Moving " + j);
 						agents.get(j).setColumn(pc2);
 						grid[pr][pc2][0] = j;
 						grid[pr][pc][0] = -1;
@@ -76,6 +96,7 @@ public class ACO {
 				}
 				if(dir == 2){
 					if(grid[pr1][pc][0] == -1){
+						//System.out.println("Moving " + j);
 						agents.get(j).setRow(pr1);
 						grid[pr1][pc][0] = j;
 						grid[pr][pc][0] = -1;
@@ -86,6 +107,7 @@ public class ACO {
 				}
 				if(dir == 3){
 					if(grid[pr2][pc][0] == -1){
+						//System.out.println("Moving "+j);
 						agents.get(j).setRow(pr2);
 						grid[pr2][pc][0] = j;
 						grid[pr][pc][0] = -1;
@@ -93,6 +115,7 @@ public class ACO {
 						dir = 0;
 						if (dir == 0) {
 							if (grid[pr][pc1][0] == -1) {
+								//System.out.println("Moving " + j);
 								agents.get(j).setColumn(pc1);
 								grid[pr][pc1][0] = j;
 								grid[pr][pc][0] = -1;
@@ -102,6 +125,7 @@ public class ACO {
 						}
 						if(dir == 1){
 							if(grid[pr][pc2][0] == -1){
+								//System.out.println("Moving " + j);
 								agents.get(j).setColumn(pc2);
 								grid[pr][pc2][0] = j;
 								grid[pr][pc][0] = -1;
@@ -111,6 +135,7 @@ public class ACO {
 						}
 						if(dir == 2){
 							if(grid[pr1][pc][0] == -1){
+								//System.out.println("Moving " + j);
 								agents.get(j).setRow(pr1);
 								grid[pr1][pc][0] = j;
 								grid[pr][pc][0] = -1;
@@ -123,7 +148,7 @@ public class ACO {
 
 				// l = does agent j carry an item?
 				boolean l = false;
-				if (!agents.get(j).getCarried().equals(null)) {
+				if (agents.get(j).getCarried() != null) {
 					l = true;
 				}
 				// e = is current position occupied by item?
@@ -137,12 +162,13 @@ public class ACO {
 					boolean d = false;
 					// drop i if probability says so
 					double pDrop = 1;
-					double fi = evaluateFitness(data, agents.get(j).getCarried(), agents.get(j).getRow(),
-							agents.get(j).getColumn());
+					//System.out.println("Dropping?");
+					double fi = evaluateFitness(data, agents.get(j).getCarried(), agents.get(j).getRow(), agents.get(j).getColumn());
 					if (fi <= kMinus) {
 						pDrop = 2 * fi;
 					}
 					if ((Math.random()) <= pDrop) {
+						System.out.println("Dropping");
 						drop(agents.get(j));
 					}
 				}
@@ -151,16 +177,17 @@ public class ACO {
 					// i = object at position
 					// boolean p = false;
 					// pick up i if probability says so
-					double pPick = Math.pow((kPlus / (kPlus + evaluateFitness(data, agents.get(j).getCarried(),
-							agents.get(j).getRow(), agents.get(j).getColumn()))), 2);
+					System.out.println("Picking Up?");
+					double pPick = Math.pow((kPlus / (kPlus + evaluateFitness(data, data.get(grid[agents.get(j).getRow()][agents.get(j).getColumn()][1]), agents.get(j).getRow(), agents.get(j).getColumn()))), 2);
 					if ((Math.random()) <= pPick) {
+						System.out.println("PickingUp");
 						pickUp(agents.get(j), data.get(grid[agents.get(j).getRow()][agents.get(j).getColumn()][1]));
 					}
 
 				}
 			}
 		}
-		clusters = clusterAssignment(grid, clusters);// will take grid at some point
+		clusters = clusterAssignment(grid, clusters, data);// will take grid at some point
 		return clusters;
 	}
 
@@ -181,9 +208,10 @@ public class ACO {
 			for (int j = 0 - neighborhoodSize; j <= neighborhoodSize; j++) {
 				int posRow = Math.floorMod((row + i), gridRowSize);
 				int posCol = Math.floorMod((col + j), gridRowSize);
+				//System.out.println("posRow: " + posRow + ", posCol:" + posCol);
 				if (grid[posRow][posCol][1] != -1 && i != 0 && j != 0) {
-					double tempF = d.calculateDistance(dat.getData(), data.get(grid[posRow][posCol][1]).getData(),
-							dat.getData().size());
+					//System.out.println("Dat: " + dat.getData() + ", grid result: " + grid[posRow][posCol][1]);
+					double tempF = d.calculateDistance(dat.getData(), data.get(grid[posRow][posCol][1]).getData(), dat.getData().size());
 					tempF = tempF / alpha;
 					tempF = 1 - tempF;
 					sumF += tempF;
@@ -226,7 +254,7 @@ public class ACO {
 		return agents;
 	}
 
-	private ArrayList<Cluster> clusterAssignment(int[][][] grid, ArrayList<Cluster> clusters) {// will take toroidal grid and assign data to clusters
+	private ArrayList<Cluster> clusterAssignment(int[][][] grid, ArrayList<Cluster> clusters, ArrayList<Datum> data) {// will take toroidal grid and assign data to clusters
 		ArrayList<Datum> newData = new ArrayList<Datum>();
 		for(int i = 0; i < gridRowSize; i++){
 			for(int j = 0; j < gridColSize; j++){
@@ -234,12 +262,21 @@ public class ACO {
 					ArrayList<Double> in = new ArrayList<Double>();
 					in.add((double)i);
 					in.add((double)j);
-					newData.add(grid[i][j][1], new Datum(in));
+					in.add((double)grid[i][j][1]);
+					//System.out.println(grid[i][j][1]);
+					newData.add(new Datum(in));
 				}
 			}
 		}
-		KMeans kmeans = new KMeans(k, gens);
+		KMeans kmeans = new KMeans(5, 300);
 		clusters = kmeans.run(newData);
-		return clusters;
+		ArrayList<Cluster> clusters2 = new ArrayList<Cluster>();
+		for(int i = 0; i < clusters.size(); i++){
+			clusters2.add(new Cluster(clusters.get(i).getCentroid(), i));
+			for(int j = 0; j < clusters.get(i).getPts().size(); j++){
+				clusters2.get(i).addPoint(data.get((int) Math.floor(clusters.get(i).getPts().get(j).getData().get(2))));
+			}
+		}
+		return clusters2;
 	}
 }
